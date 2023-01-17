0c1d71ba515d527d42ea5cf7c9bf852fb2ccbc7e
JoeGitST <joe.hatier@st.com>
2023-01-17 18:59:08 +0100

    minor modif ReadMe

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,6 +36,8 @@ const Header = (props) => {
                 }, {
                     namePrefix: "p2pS_"           // P2P : Server 
                 }, {
+                    namePrefix: "P2PS_"           // P2P : Server 
+                }, {
                     namePrefix: "p2pR_"           // P2P : Router
                 }, {
                     namePrefix: "p2pSext_"        // P2P : Server Ext
@@ -44,7 +46,7 @@ const Header = (props) => {
                 }],
 
                 optionalServices: ['0000fe40-cc7a-482a-984a-7f2ed5b3e58f', '0000180d-0000-1000-8000-00805f9b34fb','0000fe80-cc7a-482a-984a-7f2ed5b3e58f','0000fe80-8e22-4541-9d4c-21edae82fe80','0000fe20-cc7a-482a-984a-7f2ed5b3e58f', '0000feb0-cc7a-482a-984a-7f2ed5b3e58f'] // service uuid of [P2P service, Heart Rate service, DataThroughput, Ota]
-            })  
+            }) 
                 .then(device => { 
                     myDevice = device;
                     myDevice.addEventListener('gattserverdisconnected', onDisconnected);     
