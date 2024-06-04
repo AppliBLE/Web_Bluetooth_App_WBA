00b6de0df46e27d75e715347528aa0b0953b6645
LEBLEUAD <adrien.lebleu01@st.com>
2024-06-04 14:24:33 +0200

    last Wifi/BLE app

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,7 +53,7 @@ const Header = (props) => {
                     namePrefix: "HUB_DYN"      // HUB Zigbee
                 }],
 
-                optionalServices: ['0000fe40-cc7a-482a-984a-7f2ed5b3e58f', '0000180d-0000-1000-8000-00805f9b34fb','0000fe80-cc7a-482a-984a-7f2ed5b3e58f','0000fe80-8e22-4541-9d4c-21edae82fe80','0000fe20-cc7a-482a-984a-7f2ed5b3e58f', '0000feb0-cc7a-482a-984a-7f2ed5b3e58f', '00001809-0000-1000-8000-00805f9b34fb', '00000000-cc7a-482a-984a-7f2ed5b3e58f', '0000fe90-cc7a-482a-984a-7f2ed5b3e58f'] // service uuid of [P2P service, Heart Rate service, DataThroughput, Ota, P2P Router, Health Thermomiter]
+                optionalServices: ['0000fe40-cc7a-482a-984a-7f2ed5b3e58f', '0000180d-0000-1000-8000-00805f9b34fb','0000fe80-cc7a-482a-984a-7f2ed5b3e58f','0000fe80-8e22-4541-9d4c-21edae82fe80','0000fe20-cc7a-482a-984a-7f2ed5b3e58f', '0000feb0-cc7a-482a-984a-7f2ed5b3e58f', '00001809-0000-1000-8000-00805f9b34fb','d973f2e0-b19e-11e2-9e96-0800200c9a66', '0000fec0-cc7a-482a-984a-7f2ed5b3e58f', '0000ff9a-cc7a-482a-984a-7f2ed5b3e58f', '00000000-cc7a-482a-984a-7f2ed5b3e58f']  // service uuid of [P2P service, Heart Rate service, DataThroughput, Ota, P2P Router, Health Thermomiter, Finger Print]
             }) 
                 .then(device => { 
                     myDevice = device;
